2f6f23339878e450cbf955566be682d81c67d0e5
uncoolike <uncoolike@gmail.com>
2021-01-20 10:54:32 -0600

    Add project files.

---
+++ SAT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SAT.Startup))]
+namespace SAT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
