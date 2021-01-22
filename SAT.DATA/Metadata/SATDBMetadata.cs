4d1890d3ac08e55aa73038b46ef1f983cbf1ced9
S-Toney <stacy.toney@protonmail.com>
2021-01-22 12:27:26 -0600

    metadata notations added

--- SAT.DATA/Metadata/SATDBMetadata.cs
+++ SAT.DATA/Metadata/SATDBMetadata.cs
@@ -10,17 +10,22 @@ namespace SAT.DATA//.Metadata
     
     public partial class CourseMetadata
     {
-        [Display(Name = "Course ID")]
-        public int CourseID { get; set; }
+        //[Required(ErrorMessage ="* Course ID is required")]
+        //[Display(Name = "Course ID")]
+        //public int CourseID { get; set; }
+        [Required(ErrorMessage = "* Course Name is required")]
         [Display(Name ="Course Name")]
         public string CourseName { get; set; }
+        [Required(ErrorMessage = "* Course Description is required")]
         [Display(Name ="Description")]
         public string CourseDescription { get; set; }
+        [Required(ErrorMessage = "* Credit Hours are required")]
         [Display(Name ="Credit Hours")]
         public byte CreditHours { get; set; }
         [Display(Name ="Required Materials")]
         public string Curriculum { get; set; }
         public string Notes { get; set; }
+        [Required(ErrorMessage = "* Active status is required")]
         [Display(Name ="Active")]
         public bool IsActive { get; set; }
     }
@@ -30,12 +35,16 @@ namespace SAT.DATA//.Metadata
 
     public partial class EnrollmentMetadata
     {
-        [Display(Name ="Enrollment ID")]
-        public int EnrollmentID { get; set; }
+        //[Required(ErrorMessage = "* Enrollment ID is required")]
+        //[Display(Name ="Enrollment ID")]
+        //public int EnrollmentID { get; set; }
+        [Required(ErrorMessage = "* Student ID is required")]
         [Display(Name ="Student ID")]
         public int StudentID { get; set; }
+        [Required(ErrorMessage = "* Scheduled Class ID is required")]
         [Display(Name ="Scheduled Class ID")]
         public int ScheduledClassID { get; set; }
+        [Required(ErrorMessage = "* Enrollment Date is required")]
         [Display(Name ="Enrollment Date")]
         public System.DateTime EnrollmentDate { get; set; }
     }
@@ -45,17 +54,24 @@ namespace SAT.DATA//.Metadata
 
     public class ScheduledClassMetadata
     {
-        [Display(Name = "Scheduled Class ID")]
-        public int ScheduledClassID { get; set; }
+        //[Required(ErrorMessage = "* Scheduled Class ID is required")]
+        //[Display(Name = "Scheduled Class ID")]
+        //public int ScheduledClassID { get; set; }
+        [Required(ErrorMessage = "* Course ID is required")]
         [Display(Name = "Course ID")]
         public int CourseID { get; set; }
+        [Required(ErrorMessage = "* Start Date is required")]
         [Display(Name ="Start Date")]
         public System.DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "* End Date is required")]
         [Display(Name ="End Date")]
         public System.DateTime EndDate { get; set; }
+        [Required(ErrorMessage = "* Instructor is required")]
         [Display(Name ="Instructor")]
         public string InstructorName { get; set; }
+        [Required(ErrorMessage = "* Location is required")]
         public string Location { get; set; }
+        [Required(ErrorMessage = "* Scheduled Class Status ID is required")]
         public int SCSID { get; set; }
     }
     [MetadataType(typeof(ScheduledClassMetadata))]
@@ -63,7 +79,9 @@ namespace SAT.DATA//.Metadata
 
     public partial class ScheduledClassStatusMetadata
     {
-        public int SCSID { get; set; }
+        //[Required(ErrorMessage = "* Scheduled Class Status ID is required")]
+        //public int SCSID { get; set; }
+        [Required(ErrorMessage = "* Status Class Status is required")]
         [Display(Name ="Status")]
         public string SCSName { get; set; }
     }
@@ -73,10 +91,13 @@ namespace SAT.DATA//.Metadata
 
     public partial class StudentMetadata
     {
-        [Display(Name ="Student ID")]
-        public int StudentID { get; set; }
+        //[Required(ErrorMessage = "* Student ID is required")]
+        //[Display(Name ="Student ID")]
+        //public int StudentID { get; set; }
+        [Required(ErrorMessage = "* Student First Name is required")]
         [Display(Name ="First Name")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "* Student Last Name is required")]
         [Display(Name ="Last Name")]
         public string LastName { get; set; }
         public string Major { get; set; }
@@ -85,9 +106,11 @@ namespace SAT.DATA//.Metadata
         public string State { get; set; }
         public string ZipCode { get; set; }
         public string Phone { get; set; }
+        [Required(ErrorMessage = "* Student Email is required")]
         public string Email { get; set; }
         [Display(Name ="Photo")]
         public string PhotoUrl { get; set; }
+        [Required(ErrorMessage = "* Student Status ID is required")]
         public int SSID { get; set; }
     }
 
@@ -103,7 +126,8 @@ namespace SAT.DATA//.Metadata
 
     public partial class StudentStatusMetadata
     {
-        public int SSID { get; set; }
+        //public int SSID { get; set; }
+        [Required(ErrorMessage = "* Student Status is required")]
         [Display(Name ="Status")]
         public string SSName { get; set; }
         [Display(Name ="Description")]
